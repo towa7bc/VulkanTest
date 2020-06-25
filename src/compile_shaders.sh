ba132d9f422c60be5bac7d16ce3e20d426fee966
MW <michi.wittmann@gmail.com>
2020-06-25 14:21:51 +0200

    Full tutorial

--- src/compile_shaders.sh
+++ src/compile_shaders.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-glslc -fshader-stage=vertex  vert.glsl -o vert.spv
-glslc -fshader-stage=fragment  frag.glsl -o frag.spv
-glslc -fshader-stage=vertex  vert2.glsl -o vert2.spv
-glslc -fshader-stage=fragment  frag2.glsl -o frag2.spv
+glslc -fshader-stage=vertex vert.glsl -o vert.spv
+glslc -fshader-stage=fragment frag.glsl -o frag.spv
+glslc -fshader-stage=vertex vert2.glsl -o vert2.spv
+glslc -fshader-stage=fragment frag2.glsl -o frag2.spv
